7ed821448b08e6a82fb2d4749c0e83e24f10ea10
Daniel Martín García <danielmartingarcia@outlook.es>
2019-12-26 17:12:02 +0100

    Deploy minor changes

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ cp ${DIRECTORY}/openstack_lab.xml .
 sudo vnx -f openstack_lab.xml --create
 sudo vnx -f openstack_lab.xml -x start-all
 sudo vnx -f openstack_lab.xml -x load-img
-vnx_config_nat ExtNet enp2s0
+vnx_config_nat ExtNet enp1s0
 echo "Escenario desplegado"
