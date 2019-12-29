1c809b6afaab19993b1f8d18436a9c2d37c510d9
Daniel Martín García <danielmartingarcia@outlook.es>
2019-12-29 15:59:46 +0100

    Network creation

--- script.sh
+++ script.sh
@@ -8,3 +8,8 @@ openstack stack output show stack2 instance_ip -c output_value
 openstack stack output show stack2 instance_ip -f json
 
 openstack stack output show stack2 instance_ip -f value -c output_value #saca IP
+
+
+# NETWOKS
+openstack stack create -t net.yml --parameter "net_name=net1" --parameter "subnet_name=subnet1" --parameter "start_allocation_pools=10.1.1.8" --parameter "end_allocation_pools=10.1.1.100" --parameter "gateway_ip=10.1.1.1" --parameter "subnet_cidr=10.1.1.0/24" net1_stack
+openstack stack create -t net.yml --parameter "net_name=net2" --parameter "subnet_name=subnet2" --parameter "start_allocation_pools=10.1.2.8" --parameter "end_allocation_pools=10.1.2.100" --parameter "gateway_ip=10.1.2.1" --parameter "subnet_cidr=10.1.2.0/24" net2_stack
